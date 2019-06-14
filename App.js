206be68683d97125f853f4ff56d5efc27b469ee0
jayme schmid <jayme.schmid@gmail.com>
2019-06-13 19:10:40 -0700

    basic bindings woring

--- App.js
+++ App.js
@@ -6,94 +6,126 @@
  * @flow
  */
 import React from 'react';
-import { View, Text, TextInput,Picker, Button,StyleSheet } from 'react-native';
+
+import { View, Text, TouchableOpacity, TextInput, Picker, Button, StyleSheet, style, br } from 'react-native';
 import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
 
 class LoginScreen extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     //1
     super(props);
-  //2
-    this.state={userName:"",password:""};
-  
+    //2
+    state = { userName: " ", password: " " };
+
     //3
-    this.handlechange= this.handlechange.bind(this);
-    this.validateUser= this.validateUser.bind(this);
-  //4:user  data 
-  
-  
-  
-  this.users=[
-    {userName:"aaa",passhash:"abc"},
-    {userName:"bb",passhash:"123"}
-  ];
-  
+    this.handlechange = this.handlechange.bind(this);
+    this.validateUser = this.validateUser.bind(this);
+    //4:user  data 
+
+
+
+    this.users = [
+      { userName: "aaa", passhash: "abc" },
+      { userName: "bb", passhash: "123" }
+    ];
+
   }
-  
+
   //on change listener 
-  handlechange(value){
-  
+  handlechange(value) {
+
     //this.setState({"userName":value});
-    
 
- /* if(e.target.id==="U1"){
-    this.setState({"userName":""});
+
+    /* if(e.target.id==="U1"){
+       this.setState({"userName":""});
+     }
+     else if(e.target.id==="P1"){
+       this.setState({"password":e.target.value});
+     }*/
   }
-  else if(e.target.id==="P1"){
-    this.setState({"password":e.target.value});
-  }*/
-  }
-  
-  validateUser(){
-  
-  /*fetch("http://tiny.cc/y7qd7y",{mode:'no-cors'})
-  /.then(red =>resizeBy.json())
-  .then(
-    (result)=>{
-      console.log(result);
-    },
-    
-  )*/
 
-  
-  this.props.navigation.dispatch(StackActions.reset({
-    index: 0,
-    actions: [
-      NavigationActions.navigate({ routeName: 'Main' })
-    ],
-  }))
+  validateUser() {
 
-
-if("a"===this.setState.userName){
-  
+    /*fetch("http://tiny.cc/y7qd7y",{mode:'no-cors'})
+    /.then(red =>resizeBy.json())
+    .then(
+      (result)=>{
+        console.log(result);
+      },
+      
+    )*/
+   // console.log(this.state.userName);
+    //console.log(this.state.password);
+    try{
+if(Object.keys(this.state.userName).length==0) {
+console.log("username is blank "); 
 }
+else  if(Object.keys(this.state.password).length==0) {
+  console.log("password  is blank "); 
+  }
 
- 
-  /*
-   console.log("xxxx",this.state.userName);
-  
-  
-  let isuservalid= false; 
-  this.users.forEach(users =>{
-  
-  
-  if(users.userName===this.setState.userName&& users.passhash===this.state.password){
-    isuservalid= true;
-    console.log("user is valid ");
-  
-  }
-  else 
-  {
-    console.log("user is invalid ");
-  }
-  
-  });
-  
-  */
-  
-  
-  
+else if( this.state.userName=="A1"){
+console.log("valid username")
+if(this.state.password=="A2"){
+console.log("valid  password ");
+this.props.navigation.dispatch(StackActions.reset({
+  index: 0,
+  actions: [
+    NavigationActions.navigate({ routeName: 'Main' })
+  ],
+}))
+}
+else{
+  console.log( "invalid password");
+}
+}
+else{
+console.log("invalid username");
+}
+    }
+    catch(e){
+      console.log(e); 
+    }
+
+
+
+    /*
+
+
+
+    this.props.navigation.dispatch(StackActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'Main' })
+      ],
+    }))
+
+
+    if ("a" === this.setState.userName) {
+
+    }*/
+
+
+    /*
+     console.log("xxxx",this.state.userName);
+    
+    
+    let isuservalid= false; 
+    this.users.forEach(users =>{
+    
+    
+    if(users.userName===this.setState.userName&& users.passhash===this.state.password){
+      isuservalid= true;
+      console.log("user is valid ");
+    
+    }
+    els
+    */
+
+
+
   }
 
 
@@ -102,38 +134,40 @@ if("a"===this.setState.userName){
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-   
-    <TextInput  placeholder="User Name"  id="U1" onChangeText={(userName) =>this.setState({userName})} />
-    <TextInput  placeholder="Password"  id="P1"  onChangeText={(password) =>this.setState({password})}  />
+      <View style={styles.MainContiner}>
+        <Text style={styles.Header}>Home Screen</Text>
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <TextInput placeholder="User Name" id="U1" onChangeText={(userName) => this.setState({ userName })} />
 
-        <Button
-          title="Login"
-          onPress={this.validateUser.bind(this)}
-         /* onPress={() => {
-            this.props.navigation.dispatch(StackActions.reset({
-              index: 0,
-              actions: [
-                NavigationActions.navigate({ routeName: 'Main' })
-              ],
-            }))
-          }}*/
-        />
-        <Button
-          title="Go to Register"
-          onPress={() => {
-            this.props.navigation.dispatch(StackActions.reset({
-              index: 0,
-              actions: [
-                NavigationActions.navigate({ routeName: 'Register' })
-              ],
-            }))
-          }}
-        />
+          <TextInput placeholder="Password" id="P1" onChangeText={(password) => this.setState({ password })} />
+          <Text> Login </Text>
+
+          <TouchableOpacity
+            style={styles.ButtonStlye}
+            onPress={this.validateUser.bind(this)}
+
+          /> 
+
+          <TouchableOpacity
+
+            style={styles.ButtonStlye}
+            onPress={() => {
+              this.props.navigation.dispatch(StackActions.reset({
+                index: 0,
+                actions: [
+                  NavigationActions.navigate({ routeName: 'Register' })
+                ],
+              }))
+            }}
+          /><Text > go to register </Text> 
+
+
+
+          
+        </View>
       </View>
     );
-  }  
+  }
 }
 class RegisterScreen extends React.Component {
   render() {
@@ -142,22 +176,22 @@ class RegisterScreen extends React.Component {
         <Text>Register</Text>
 
         <Text>User Name:</Text>
-        <TextInput  placeholder="User Name"  id="U1" onChangeText={(userName) =>this.setState({userName})} />
-   
+        <TextInput placeholder="User Name" id="U1" onChangeText={(userName => { this.setState({userName}); })} />
+
         <Text>Password:</Text>
-    <TextInput  placeholder="Password"  id="P1"  onChangeText={(password) =>this.setState({password})}  />
-   
-    <Text>Location:</Text>
-    <Picker
- //selectedValue={this.state.language}
-  style={{height: 50, width: 100}}
-  onValueChange={(itemValue, itemIndex) =>
-    this.setState({language: itemValue})
-  }>
-  <Picker.Item label="aud" value="aud" />
-  <Picker.Item label="euro" value="euro" />
-  <Picker.Item label="usd" value="usd" />
-</Picker>
+        <TextInput placeholder="Password" id="P1" onChangeText={(password => { this.setState({password}); })} />
+
+        <Text>Location:</Text>
+        <Picker
+          //selectedValue={this.state.language}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ language: itemValue })
+          }>
+          <Picker.Item label="aud" value="aud" />
+          <Picker.Item label="euro" value="euro" />
+          <Picker.Item label="usd" value="usd" />
+        </Picker>
 
 
         <Button
@@ -171,7 +205,7 @@ class RegisterScreen extends React.Component {
             }))
           }}
         />
-         <Button
+        <Button
           title="Register "
           onPress={() => {
             this.props.navigation.dispatch(StackActions.reset({
@@ -184,7 +218,7 @@ class RegisterScreen extends React.Component {
         />
       </View>
     );
-  }  
+  }
 }
 
 class MainScreen extends React.Component {
@@ -203,7 +237,7 @@ class MainScreen extends React.Component {
             }))
           }}
         />
-          <Button
+        <Button
           title="watch crypto currencies "
           onPress={() => {
             this.props.navigation.dispatch(StackActions.reset({
@@ -214,7 +248,7 @@ class MainScreen extends React.Component {
             }))
           }}
         />
-          <Button
+        <Button
           title="track youre investment"
           onPress={() => {
             this.props.navigation.dispatch(StackActions.reset({
@@ -225,7 +259,7 @@ class MainScreen extends React.Component {
             }))
           }}
         />
-          <Button
+        <Button
           title="settings "
           onPress={() => {
             this.props.navigation.dispatch(StackActions.reset({
@@ -236,7 +270,7 @@ class MainScreen extends React.Component {
             }))
           }}
         />
-          <Button
+        <Button
           title="Back to Login "
           onPress={() => {
             this.props.navigation.dispatch(StackActions.reset({
@@ -249,7 +283,7 @@ class MainScreen extends React.Component {
         />
       </View>
     );
-  }  
+  }
 }
 class SettingsScreen extends React.Component {
   render() {
@@ -260,32 +294,32 @@ class SettingsScreen extends React.Component {
 
         <Text>Location</Text>
         <Picker
- //selectedValue={this.state.language}
-  style={{height: 50, width: 100}}
-  onValueChange={(itemValue, itemIndex) =>
-    this.setState({language: itemValue})
-  }>
-  <Picker.Item label="aud" value="aud" />
-  <Picker.Item label="euro" value="euro" />
-  <Picker.Item label="usd" value="usd" />
-</Picker>
+          //selectedValue={this.state.language}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ language: itemValue })
+          }>
+          <Picker.Item label="aud" value="aud" />
+          <Picker.Item label="euro" value="euro" />
+          <Picker.Item label="usd" value="usd" />
+        </Picker>
 
 
 
-<Text>notifications </Text>
+        <Text>notifications </Text>
 
 
-<Text>notification freqency</Text>
-<Picker
- //selectedValue={this.state.language}
-  style={{height: 50, width: 100}}
-  onValueChange={(itemValue, itemIndex) =>
-    this.setState({language: itemValue})
-  }>
-  <Picker.Item label="aud" value="aud" />
-  <Picker.Item label="euro" value="euro" />
-  <Picker.Item label="usd" value="usd" />
-</Picker>
+        <Text>notification freqency</Text>
+        <Picker
+          //selectedValue={this.state.language}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ language: itemValue })
+          }>
+          <Picker.Item label="aud" value="aud" />
+          <Picker.Item label="euro" value="euro" />
+          <Picker.Item label="usd" value="usd" />
+        </Picker>
 
 
         <Button
@@ -301,7 +335,7 @@ class SettingsScreen extends React.Component {
         />
       </View>
     );
-  }  
+  }
 }
 class GeneralScreen extends React.Component {
   render() {
@@ -321,7 +355,7 @@ class GeneralScreen extends React.Component {
         />
       </View>
     );
-  }  
+  }
 }
 class WatchMScreen extends React.Component {
   render() {
@@ -341,7 +375,7 @@ class WatchMScreen extends React.Component {
         />
       </View>
     );
-  }  
+  }
 }
 class TrackScreen extends React.Component {
   render() {
@@ -372,7 +406,7 @@ class TrackScreen extends React.Component {
         />
       </View>
     );
-  }  
+  }
 }
 class InvAddScreen extends React.Component {
   render() {
@@ -386,22 +420,22 @@ class InvAddScreen extends React.Component {
         <Text>currency type</Text>
 
         <Picker
- //selectedValue={this.state.language}
-  style={{height: 50, width: 100}}
-  onValueChange={(itemValue, itemIndex) =>
-    this.setState({language: itemValue})
-  }>
-  <Picker.Item label="aud" value="aud" />
-  <Picker.Item label="euro" value="euro" />
-  <Picker.Item label="usd" value="usd" />
-</Picker>
+          //selectedValue={this.state.language}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ language: itemValue })
+          }>
+          <Picker.Item label="aud" value="aud" />
+          <Picker.Item label="euro" value="euro" />
+          <Picker.Item label="usd" value="usd" />
+        </Picker>
 
         <Text>ammount of coins </Text>
-        <TextInput  placeholder="Password"  id="P1"  onChangeText={(password) =>this.setState({password})}  />
-   
+        <TextInput placeholder="Password" id="P1" onChangeText={(password) => this.setState({ password })} />
+
         <Text>purchase price</Text>
-        <TextInput  placeholder="Password"  id="P1"  onChangeText={(password) =>this.setState({password})}  />
-   
+        <TextInput placeholder="Password" id="P1" onChangeText={(password) => this.setState({ password })} />
+
         <Text>notifications </Text>
 
 
@@ -424,7 +458,7 @@ class InvAddScreen extends React.Component {
         />
 
 
-<Button
+        <Button
           title="add investment "
           onPress={() => {
             this.props.navigation.dispatch(StackActions.reset({
@@ -437,7 +471,7 @@ class InvAddScreen extends React.Component {
         />
       </View>
     );
-  }  
+  }
 }
 
 const AppNavigator = createStackNavigator({
@@ -453,10 +487,10 @@ const AppNavigator = createStackNavigator({
   Settings: {
     screen: SettingsScreen,
   },
- GeneralMarket: {
+  GeneralMarket: {
     screen: GeneralScreen,
   },
- WatchMarket: {
+  WatchMarket: {
     screen: WatchMScreen,
   },
   InvTrack: {
@@ -467,11 +501,45 @@ const AppNavigator = createStackNavigator({
   },
 }, {
     initialRouteName: 'Login',
-});
+  });
 
 export default createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
+  MainContiner: {
+
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#f5f5dc',
+    height: 300,
+
+
+  },
+  ButtonStlye: {
+
+    alignItems: 'center',
+    padding: 10,
+    marginBottom: 10,
+    width: 200,
+    height: 100,
+    backgroundColor: '#505050',
+
+
+
+  },
+
+
+
+  Header: {
+
+    justifyContent: 'center',
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#565652',
+    backgroundColor: '#f5f5dc',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
